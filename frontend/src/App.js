f75eb4eac95562746d0d7e3a18f0b7be19cd0c7a
sejal jaswal <sejaljaswal@sejals-MacBook-Air-6.local>
2025-02-06 09:54:21 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {LoginPage} from "./Routess.js"// Import fixed
+import {LoginPage,SignUpPage} from "./Routess.js"// Import fixed
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
