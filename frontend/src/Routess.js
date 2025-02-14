458ed81526d9aaa61494a8b194151d221a803eeb
sejal jaswal <sejaljaswal@sejals-MacBook-Air-8.local>
2025-02-14 10:30:40 +0530

    done

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -1,8 +1,9 @@
 import LoginPage from "../src/pages/login.jsx"
 import SignUpPage from "../src/pages/SignUpPage.jsx"
-
+import ActivationPage from "../src/pages/ActivationPage.jsx";
 export
 {
     LoginPage,
-    SignUpPage
+    SignUpPage,
+    ActivationPage
 }
