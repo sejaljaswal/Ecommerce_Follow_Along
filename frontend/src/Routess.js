f2ca27fad508fb49eafe5530408b3385a5d70645
sejal jaswal <sejaljaswal@sejals-MacBook-Air-9.local>
2025-02-24 10:53:57 +0530

    done

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -1,9 +1,9 @@
-import LoginPage from "../src/pages/Login.jsx"
+import LoginPage from "./pages/Login.jsx"
 import SignUpPage from "../src/pages/SignUpPage.jsx"
 import ActivationPage from "../src/pages/ActivationPage.jsx";
 import Home from "../src/pages/Home.jsx"
-import CreateProduct from "../src/pages/createProduct.jsx"
-import MyProducts from "../src/pages/myProducts.jsx";
+import CreateProduct from "../src/pages/ createProduct.jsx"
+import MyProducts from "../src/pages/ myProducts.jsx";
 export
 {
     LoginPage,
