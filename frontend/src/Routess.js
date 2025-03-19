232e5a00a8977d9682565d1c2a0c2d33c50ae2d5
Sejal <sejaljaswal2020@gmail.com>
2025-03-19 11:31:54 +0530

    done

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -6,6 +6,7 @@ import CreateProduct from "../src/pages/ createProduct.jsx"
 import MyProducts from "../src/pages/ myProducts.jsx";
 import Cart from "./pages/cart.jsx";
 import ProductDetails from "./pages/productDetails.jsx";
+import Profile from "./pages/profile.jsx";
 export
 {
     LoginPage,
@@ -15,6 +16,7 @@ export
     CreateProduct,
     MyProducts,
     Cart,
-    ProductDetails
+    ProductDetails,
+    Profile
 
 }
