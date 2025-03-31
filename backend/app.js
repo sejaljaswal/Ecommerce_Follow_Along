b9b9fa3537acc74a2a407e84efd78c88f16d0838
Sejal <sejaljaswal2020@gmail.com>
2025-03-31 09:17:51 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const ErrorHandler = require("./middleware/ error");
+const ErrorHandler = require("./middleware/error");
 
 const app = express();
 
@@ -27,11 +27,14 @@ app.use('/products', express.static(path.join(__dirname, 'products')));
 
 // Import Routes
 const userRoutes = require("./controller/user");
-const productRoutes = require('./controller/ product');
+const productRoutes = require('./controller/product');
+const orders = require('./controller/orders');
+
 
 // Route Handling
 app.use("/api/v2/user", userRoutes);
 app.use("/api/v2/product", productRoutes);
+app.use("/api/v2/orders", orders);
 
 // Error Handling Middleware
 app.use(ErrorHandler);
